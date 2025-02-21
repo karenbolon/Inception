b8b8f61d495bb1bbfbcbc564cbf414e70b8f03c9
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2025-02-21 11:04:33 +0100

    updated init.sh for linux and macos and further checks if items exist with colour

--- Makefile
+++ Makefile
@@ -1,25 +1,19 @@
-SECRETS_DIR = ./secrets
+#Linux:
+#SECRETS_DIR = "/home/kbolon/Documents/Inception/secrets"
+#DATA_DIR = "/home/kbolon/data"
+#MACOS:
+SECRETS_DIR = "/Users/karenbolon/Documents/Inception/secrets"
+DATA_DIR = "/Users/karenbolon/data"
+
 COMPOSE_FILE = srcs/docker-compose.yml
 INIT_SCRIPT = ./srcs/init.sh
-DATA_DIR = ../../data
 ENV_FILE = ./srcs/.env
 
 all: init up
 
-#/dev/null is a special file on linux systems that discards anything written to it
-# 2>&1 redirects file descripter 2 (standard error) to FD1 or (stdout) to /dev/null too
-
-#-p flag creates a parent DIR if needed & avoids errors if it exists
-make_directories:
-	@mkdir -p /home/kbolon/data/mariadb
-	@mkdir -p /home/kbolon/data/wordpress
-	@mkdir -p "/home/kbolon/Documents/Inception/secrets"
-	@mkdir -m 775 secrets
-
 init:
 	@echo "Creating .env file..."
 	@bash $(INIT_SCRIPT)
-	@echo ".env file created successfully!"
 
 #Docker compose
 up:
@@ -53,8 +47,8 @@ clean:
 	@docker compose -f $(COMPOSE_FILE) down --volumes --remove-orphans || echo "compose is not running"
 	@docker system prune -f volumes
 #	|| prevents errors in makefile, means execute RH CMD if LH CMD fails
-	@rm -fr $(SECRETS_DIR) || true
-	@rm -fr $(DATA_DIR) || true
-	@rm -fr $(ENV_FILE) || true
+#	@rm -fr $(SECRETS_DIR) || true
+#	@rm -fr $(DATA_DIR) || true
+#	@rm -fr $(ENV_FILE) || true
 
 .PHONY: all up down stop restart ps re status clean
--- srcs/init.sh
+++ srcs/init.sh
@@ -1,89 +1,79 @@
 #!/bin/bash
 
+#define colours
+GREEN='\033[32m' #'\e[32m'
+BLUE='\033[34m' #'\e[34m'
+RED='\033[31m' #'\e[31m'
+RESET='\033[0m' #'\e[0m'
+
+SECRETS_DIR="/Users/karenbolon/Documents/Inception/secrets"
+#SECRETS_DIR="/home/kbolon/Documents/Inception/secrets"
+
 #Function to generate a random password
 generate_password() {
-	openssl rand -base64 8
+	local flag="$1"
+	if [ ! -f "$flag" ]; then
+		openssl rand -base64 8 > "$flag"
+		echo -e "${GREEN}$flag created${RESET}"
+	else
+		echo -e "${RED}$flag already exists${RESET}"
+	fi	
 }
 
 #create directories
-if [ ! -d "secrets" ]; then
-	mkdir -p "/home/kbolon/Documents/Inception/secrets"
-	chmod -m 775 secrets
-	echo "secrets folder created"
-echo
-	"secrets folder already exists"
-fi
+make_directories() {
+	local path="$1"
+	local permissions="$2"
 
-if [ ! -d "/home/kbolon/data/mariadb" ]; then
-	mkdir -p "/home/kbolon/data/mariadb"
-	echo "mariadb folder created"
-echo
-	"mariadb folder already exists"
-fi
+	if [ ! -d "$path" ]; then
+		mkdir -p "$path"
+		[ -n "$permissions" ] && chmod "$permissions" "$path"
+		echo -e "${GREEN}$path: created${RESET}"
+	else
+		echo -e "${RED}$path already exists${RESET}"
+	fi
+}
 
-if [ ! -d "/home/kbolon/data/wordpress" ]; then
-	mkdir -p "/home/kbolon/data/wordpress"
-	echo "wordpress folder created"
-echo
-	"wordpress folder already exists"
-fi
 
-#create passwords for WordPress and MariaDB
-if [ ! -f "secrets/wp_user_password.txt" ]; then
-	generate_password > secrets/wp_user_password.txt
-	echo "wp_user_password created"
-else
-	echo "wp_user_password already exists"
-fi
+make_directories "$SECRETS_DIR" 700
+#MACOS
+make_directories "/Users/karenbolon/data/mariadb"
+make_directories "/Users/karenbolon/data/wordpress"
+#LINUX:
+#make_directories "/home/kbolon/data/mariadb"
+#make_directories "/home/kbolon/data/wordpress"
 
-if [ ! -f "secrets/wp_root_password.txt" ]; then
-	generate_password > secrets/wp_root_password.txt
-	echo "wp_root_password created"
-else
-	echo "wp_root_password already exists"
-fi
-
-if [ ! -f "secrets/db_user_password.txt" ]; then
-	generate_password > secrets/db_user_password.txt
-	echo "db_user_password created"
-else
-	echo "db_user_password already exists"
-fi
-
-if [ ! -f "secrets/db_root_password.txt" ]; then
-	generate_password > secrets/db_root_password.txt
-	echo "db_root_password created"
-else
-	echo "db_root_password already exists"
-fi
+generate_password "$SECRETS_DIR/wp_user_password.txt"
+generate_password "$SECRETS_DIR/wp_root_password.txt"
+generate_password "$SECRETS_DIR/db_user_password.txt"
+generate_password "$SECRETS_DIR/db_root_password.txt"
 
 
 #create ssl certificates if not created
-if [ ! -f "secrets/nginx.crt" ] || [ ! -f "secrets/nginx.key" ]; then
-	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout secrets/nginx.key -out secrets/nginx.crt -subj "/CN=kbolon.42.fr" 2> /dev/null
-	echo "SSL certificates created"
+if [ ! -f "$SECRETS_DIR/nginx.crt" ] || [ ! -f "$SECRETS_DIR/nginx.key" ]; then
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $SECRETS_DIR/nginx.key -out $SECRETS_DIR/nginx.crt -subj "/CN=kbolon.42.fr" 2> /dev/null
+	echo -e "${GREEN}SSL certificates created${RESET}"
 else
-	echo "SSL certificates already created"
+	echo -e "${RED}SSL certificates already created${RESET}"
 fi
 
 #create .ENV (removes previous .env if found)
-[ -f "srcs/.env" ] && rm -f srcs/.env
-
-#write a heredoc to save everything to .env
-cat <<EOF > srcs/.env
+if [ ! -f "srcs/.env" ]; then
+	#write a heredoc to save everything to .env
+	cat <<EOF > srcs/.env
 DOMAIN_NAME=kbolon.42.fr
-
 #mariadb
 DB_USER=kbolon
 DB_NAME=database
-
 #wordpress
 WP_TITLE=inception
 WP_ADMIN_NAME=kbolon
 WP_ADMIN_EMAIL=kbolon@student.42berlin.de
 WP_USER_NAME=user
 WP_USER_EMAIL=user@gmail.com
-WP_USER_ROLE=auther
+WP_USER_ROLE=author
 EOF
-
-echo ".env file has been created"
+	echo -e "${GREEN}.env file has been created${RESET}"
+else
+	echo -e "${RED}.env already exists${RESET}"
+fi
